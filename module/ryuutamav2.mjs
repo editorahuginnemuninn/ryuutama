b198719b0e3614e37914a9bab550d2cf84663fb0
rogerioloboBR <matusael5004@hotmail.com>
2023-12-21 20:12:21 -0300

    implemetação do modulo principal

--- module/ryuutamav2.mjs
+++ module/ryuutamav2.mjs
@@ -1,48 +1,49 @@
 // Import document classes.
-import { BoilerplateActor } from "./documents/actor.mjs";
-import { BoilerplateItem } from "./documents/item.mjs";
+import { RYUU } from "./config";
+import { ryuutamaActor } from "./documents/actor.mjs";
+import { ryuutamaItem } from "./documents/item.mjs";
 // Import sheet classes.
-import { BoilerplateActorSheet } from "./sheets/actor-sheet.mjs";
-import { BoilerplateItemSheet } from "./sheets/item-sheet.mjs";
+import { ryuutamaActorSheet } from "./sheets/actor-sheet.mjs";
+import { ryuutamaItemSheet } from "./sheets/item-sheet.mjs";
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
-import { BOILERPLATE } from "./helpers/config.mjs";
+import { RYUUTAMAV2 } from "./helpers/config.mjs";
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
 /* -------------------------------------------- */
 
-Hooks.once('init', async function() {
+Hooks.once('init', async function () {
 
   // Add utility classes to the global game object so that they're more easily
   // accessible in global contexts.
-  game.boilerplate = {
-    BoilerplateActor,
-    BoilerplateItem,
+  game.ryuutamav2 = {
+    ryuutamaActor,
+    ryuutamaItem,
     rollItemMacro
   };
 
   // Add custom constants for configuration.
-  CONFIG.BOILERPLATE = BOILERPLATE;
+  CONFIG.RYUUTAMAV2 = RYUUTAMAV2;
 
   /**
    * Set an initiative formula for the system
    * @type {String}
    */
   CONFIG.Combat.initiative = {
-    formula: "1d20 + @abilities.dex.mod",
-    decimals: 2
+    formula: "1d@dex + 1d@int",
+    decimals: 0
   };
 
   // Define custom Document classes
-  CONFIG.Actor.documentClass = BoilerplateActor;
-  CONFIG.Item.documentClass = BoilerplateItem;
+  CONFIG.Actor.documentClass = ryuutamaActor;
+  CONFIG.Item.documentClass = ryuutamaItem;
 
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("boilerplate", BoilerplateActorSheet, { makeDefault: true });
+  Actors.registerSheet("ryuutamav2", ryuutamaActorSheet, { makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("boilerplate", BoilerplateItemSheet, { makeDefault: true });
+  Items.registerSheet("ryuutamav2", ryuutamaItemSheet, { makeDefault: true });
 
   // Preload Handlebars templates.
   return preloadHandlebarsTemplates();
@@ -51,9 +52,22 @@ Hooks.once('init', async function() {
 /* -------------------------------------------- */
 /*  Handlebars Helpers                          */
 /* -------------------------------------------- */
-
+Handlebars.registerHelper({
+  eq: (v1, v2) => v1 === v2,
+  ne: (v1, v2) => v1 !== v2,
+  lt: (v1, v2) => v1 < v2,
+  gt: (v1, v2) => v1 > v2,
+  lte: (v1, v2) => v1 <= v2,
+  gte: (v1, v2) => v1 >= v2,
+  and() {
+    return Array.prototype.every.call(arguments, Boolean);
+  },
+  or() {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+  }
+});
 // If you need to add Handlebars helpers, here are a few useful examples:
-Handlebars.registerHelper('concat', function() {
+Handlebars.registerHelper('concat', function () {
   var outStr = '';
   for (var arg in arguments) {
     if (typeof arguments[arg] != 'object') {
@@ -62,8 +76,28 @@ Handlebars.registerHelper('concat', function() {
   }
   return outStr;
 });
+Handlebars.registerHelper("settings", function (setting) {
+  return game.settings.get("ryuutama", setting);
+});
+Handlebars.registerHelper("settingsName", function (setting) {
+  const s = game.settings.get("ryuutama", setting).replace("terrain", "terrainName").replace("weather", "weatherName");
+  if (s) {
+    return game.settings.get("ryuutama", s);
+  } else {
+    return "";
+  }
+});
 
-Handlebars.registerHelper('toLowerCase', function(str) {
+Handlebars.registerHelper("settingsDC", function (setting) {
+  const s = game.settings.get("ryuutama", setting);
+  if (s) {
+    return game.settings.get("ryuutama", s);
+  } else {
+    return 0;
+  }
+});
+
+Handlebars.registerHelper('toLowerCase', function (str) {
   return str.toLowerCase();
 });
 
@@ -71,7 +105,7 @@ Handlebars.registerHelper('toLowerCase', function(str) {
 /*  Ready Hook                                  */
 /* -------------------------------------------- */
 
-Hooks.once("ready", async function() {
+Hooks.once("ready", async function () {
   // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
   Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
 });
@@ -93,15 +127,15 @@ async function createItemMacro(data, slot) {
   const item = data.data;
 
   // Create the macro command
-  const command = `game.boilerplate.rollItemMacro("${item.name}");`;
-  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  const command = `game.ryuutamav2.rollItemMacro("${item.name}");`;
+  let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
   if (!macro) {
     macro = await Macro.create({
       name: item.name,
       type: "script",
       img: item.img,
       command: command,
-      flags: { "boilerplate.itemMacro": true }
+      flags: { "ryuutamav2.itemMacro": true }
     });
   }
   game.user.assignHotbarMacro(macro, slot);
